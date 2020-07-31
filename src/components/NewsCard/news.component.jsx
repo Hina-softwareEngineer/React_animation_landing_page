e2eaf5e440e4c8b8a78e060434218dcb592a4959
HINA KHADIM <hinakhadim2002@gmail.com>
2020-08-01 00:00:14 +0500

    adding styles

--- src/components/NewsCard/news.component.jsx
+++ src/components/NewsCard/news.component.jsx
@@ -5,16 +5,19 @@ const News = ({ image, heading }) => {
   return (
     <div className="news">
       <img src={image} alt="" />
-      <label>
-        <span>August 1, 2020</span>
-        <span>0 likes</span>
-      </label>
-      <h2>{heading}</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ullam vel
-        harum exercitationem ipsum iusto velit dicta quod suscipit officiis, aut
-        pariatur ipsam assumenda odio neque quaerat minus dignissimos molestias!
-      </p>
+      <div className="infoBox">
+        <label>
+          <span>August 1, 2020</span>
+          <span>0 likes</span>
+        </label>
+        <h2>{heading}</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ullam
+          vel harum exercitationem ipsum iusto velit dicta quod suscipit
+          officiis, aut pariatur ipsam assumenda odio neque quaerat minus
+          dignissimos molestias!
+        </p>
+      </div>
     </div>
   );
 };
